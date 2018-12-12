5dea8a60a6d0a590ce88ed8d54aa3d84e806d8b7
Cruz Antonio Cárdenas <cardenasg.cruz@gmail.com>
2018-12-12 00:33:13 -0500

    Login

---
+++ LoginSelenium.ps1
@@ -0,0 +1,19 @@
+﻿clear
+# Load the Selenium .Net library
+Add-Type -Path "C:\Users\CARDENAS MEZA\Documents\Web\selenium\lib\net45\WebDriver.dll" # or wherever your WebDriver.dll is
+ 
+# Set the PATH to ensure IEDriverServer.exe can found
+$env:PATH += ";C:\Users\CARDENAS MEZA\Documents\Web\selenium\lib\net45"
+
+$chrome_options = New-Object "OpenQA.Selenium.Chrome.ChromeOptions()"
+$chrome_options= $chrome_options.AddArguments("headless","disable-gpu")
+$chrome_object = New-Object "OpenQA.Selenium.Chrome.ChromeDriver($chrome_options)"
+$chrome_object.Navigate().GoToURL("https://app.asapconnected.com/asa.aspx" )
+$InputOrg = $chrome_object.FindElementById("ContentPlaceHolder1_txtOrgID")
+$InputOrg.SendKeys("3448")
+$InputUsr = $chrome_object.FindElementById("ContentPlaceHolder1_Username")
+$InputUsr.SendKeys("cruz.cardenas")
+$InputPwd = $chrome_object.FindElementById("ContentPlaceHolder1_Password")
+$InputPWd.SendKeys("Bash0503")
+$LoginButton = $chrome_object.FindElementById('ContentPlaceHolder1_Login') # Finding the search button
+$LoginButton.Submit() # Sending 'submit' method as .click() doesn't work
